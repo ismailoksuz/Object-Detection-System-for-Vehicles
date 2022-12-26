871cda981941994a7078aec8e862b5da17b7d8dc
Barış Hazar <barishzr3431@gmail.com>
2022-12-26 13:37:45 +0300

    Create the model and fit the data

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+from tensorflow import keras
 import matplotlib.pyplot as plt
 import numpy as np
 import PIL
 import tensorflow as tf
-
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
+AUTOTUNE = tf.data.AUTOTUNE
 
 data_dir = 'archive/cardataset'
 batch_size = 32
@@ -30,3 +30,59 @@ test_ds = tf.keras.utils.image_dataset_from_directory(
 )
 
 
+class_names = train_ds.class_names
+print(class_names)
+# Show first 10 images
+# plt.figure(figsize=(10, 10))
+# for images, labels in train_ds.take(1):
+#     for i in range(9):
+#         ax = plt.subplot(3, 3, i + 1)
+#         plt.imshow(images[i].numpy().astype("uint8"))
+#         plt.title(class_names[labels[i]])
+#         plt.axis("off")
+
+# plt.show()
+
+# To standardize values to be 0, 1 range instead of 0, 255
+normalization_layer = layers.Rescaling(1./255)
+
+# apply normalization layer to the dataset
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+# image_batch, labels_batch = next(iter(normalized_ds))
+# first_image = image_batch[0]
+# # Notice the pixel values are now in `[0,1]`.
+# print(np.min(first_image), np.max(first_image))
+
+num_classes = len(class_names)
+model = Sequential([
+    layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(
+                  from_logits=True),
+              metrics=['accuracy'])
+
+model.summary()
+
+#Train the model
+epochs=3
+history = model.fit(
+  train_ds,
+  validation_data=test_ds,
+  epochs=epochs
+)
+
+
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
